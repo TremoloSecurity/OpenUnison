fc41bd8719b692d140ceea224d6c1b1213cd0626
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2016-02-16 16:07:10 -0500

    cleaned out myfaces from the build

--- unison/open-unison-classes/src/main/java/com/tremolosecurity/openunison/OpenUnisonServletFilter.java
+++ unison/open-unison-classes/src/main/java/com/tremolosecurity/openunison/OpenUnisonServletFilter.java
@@ -43,7 +43,7 @@ public class OpenUnisonServletFilter extends UnisonServletFilter {
 
 	private SessionManager sessionManager;
 	
-	public static final String version = "1.0.6-2016021601";
+	public static final String version = "1.0.6-2016021602";
 	
 	@Override
 	public ConfigManager loadConfiguration(FilterConfig filterCfg,
