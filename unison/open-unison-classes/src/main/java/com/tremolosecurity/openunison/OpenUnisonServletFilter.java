a2a1d92c11cdf315549d06a3de44c6fccf501438
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2016-05-05 10:39:02 -0400

    updated build

--- unison/open-unison-classes/src/main/java/com/tremolosecurity/openunison/OpenUnisonServletFilter.java
+++ unison/open-unison-classes/src/main/java/com/tremolosecurity/openunison/OpenUnisonServletFilter.java
@@ -43,7 +43,7 @@ public class OpenUnisonServletFilter extends UnisonServletFilter {
 
 	private SessionManager sessionManager;
 	
-	public static final String version = "1.0.6-2016040403";
+	public static final String version = "1.0.6-2016050401";
 	
 	@Override
 	public ConfigManager loadConfiguration(FilterConfig filterCfg,
