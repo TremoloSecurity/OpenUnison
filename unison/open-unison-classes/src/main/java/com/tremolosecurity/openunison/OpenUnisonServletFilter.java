e3dae7e46be57eeac2d6ae45703145c30396df2a
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2016-08-18 10:29:02 -0400

    upped build number

--- unison/open-unison-classes/src/main/java/com/tremolosecurity/openunison/OpenUnisonServletFilter.java
+++ unison/open-unison-classes/src/main/java/com/tremolosecurity/openunison/OpenUnisonServletFilter.java
@@ -44,7 +44,7 @@ public class OpenUnisonServletFilter extends UnisonServletFilter {
 	private SessionManager sessionManager;
 	
 
-	public static final String version = "1.0.7-2016080401";
+	public static final String version = "1.0.7-2016081801";
 
 	
 	@Override
