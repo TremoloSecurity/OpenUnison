2319ff0304ba9cc7b1ed6f6f6c18565dc4b4636b
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2017-04-17 11:07:14 -0400

    for #193

---
+++ unison/unison-server-core/src/main/java/com/tremolosecurity/provisioning/customTasks/AddGroupToStore.java
@@ -0,0 +1,68 @@
+/*******************************************************************************
+ * Copyright 2017 Tremolo Security, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package com.tremolosecurity.provisioning.customTasks;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.Logger;
+
+import com.tremolosecurity.provisioning.core.ProvisioningException;
+import com.tremolosecurity.provisioning.core.User;
+import com.tremolosecurity.provisioning.core.UserStoreProvider;
+import com.tremolosecurity.provisioning.core.UserStoreProviderWithAddGroup;
+import com.tremolosecurity.provisioning.core.WorkflowTask;
+import com.tremolosecurity.provisioning.util.CustomTask;
+import com.tremolosecurity.saml.Attribute;
+
+public class AddGroupToStore implements CustomTask {
+	
+	static transient Logger logger = org.apache.logging.log4j.LogManager.getLogger(AddGroupToStore.class.getName());
+
+	List<String> names;
+	String target;
+	
+	transient WorkflowTask task;
+	
+	@Override
+	public void init(WorkflowTask task, Map<String, Attribute> params) throws ProvisioningException {
+		this.names = new ArrayList<String>(); 
+			
+		this.names.addAll(params.get("name").getValues());
+				
+				
+		this.target = params.get("target").getValues().get(0);
+		this.task = task;
+	}
+
+	@Override
+	public void reInit(WorkflowTask task) throws ProvisioningException {
+		this.task = task;
+
+	}
+
+	@Override
+	public boolean doTask(User user, Map<String, Object> request) throws ProvisioningException {
+		UserStoreProvider target =  task.getConfigManager().getProvisioningEngine().getTarget(this.target).getProvider();
+		if (target instanceof UserStoreProviderWithAddGroup) {
+			for (String name : names) {
+				((UserStoreProviderWithAddGroup)target).addGroup(task.renderTemplate(name, request), user, request);
+			}
+		} else {
+			logger.warn("Target '" + this.target + "' can not add groups");
+		}
+		return true;
+	}
+
+}
