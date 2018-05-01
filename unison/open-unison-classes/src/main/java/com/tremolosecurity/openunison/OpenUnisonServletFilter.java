7c51165cdd68c4d26ae2a715dfd8abb8a6a98589
mlbiam <marc.boorshtein@tremolosecurity.com>
2018-04-30 22:53:54 -0400

    for #297

--- unison/open-unison-classes/src/main/java/com/tremolosecurity/openunison/OpenUnisonServletFilter.java
+++ unison/open-unison-classes/src/main/java/com/tremolosecurity/openunison/OpenUnisonServletFilter.java
@@ -47,7 +47,7 @@ public class OpenUnisonServletFilter extends UnisonServletFilter {
 
 
 
-	public static final String version = "1.0.13.1-2018032401";
+	public static final String version = "1.0.14-2018032401";
 
 
 
