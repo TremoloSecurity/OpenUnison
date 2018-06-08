854cac9b88ae88b46e112dc3e71361ff9d8e0697
mlbiam <marc.boorshtein@tremolosecurity.com>
2018-06-08 13:56:10 -0400

    for #308 created new cookie implementation so this can be done

--- unison/open-unison-classes/src/main/java/com/tremolosecurity/openunison/OpenUnisonServletFilter.java
+++ unison/open-unison-classes/src/main/java/com/tremolosecurity/openunison/OpenUnisonServletFilter.java
@@ -47,7 +47,7 @@ public class OpenUnisonServletFilter extends UnisonServletFilter {
 
 
 
-	public static final String version = "1.0.14.1-2018052201";
+	public static final String version = "1.0.14.1-2018052901";
 
 
 
