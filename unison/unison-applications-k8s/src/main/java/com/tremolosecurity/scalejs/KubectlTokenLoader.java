8a9e2309c34c9380261e8a21ebcc126bfaea6116
mlbiam <marc@tremolo.io>
2020-04-06 21:37:29 -0400

    for #443 added checks

--- unison/unison-applications-k8s/src/main/java/com/tremolosecurity/scalejs/KubectlTokenLoader.java
+++ unison/unison-applications-k8s/src/main/java/com/tremolosecurity/scalejs/KubectlTokenLoader.java
@@ -124,26 +124,41 @@ public class KubectlTokenLoader implements TokenLoader {
             templateObjects.put("user",user);
             templateObjects.put("token",token);
             templateObjects.put("user_id",user.getAttribs().get(this.uidAttributeName).getValues().get(0));
+            
+            String kubectTemplateLocal = this.kubectlTemplate;
+            String winKubCtlLocal = this.kubectlWinUsage;
             if (k8sCert != null) {
             	templateObjects.put("k8s_b64_cert", new String(Base64.encodeBase64(k8sCert.getBytes("UTF-8"))));
             	templateObjects.put("k8s_newline_cert",k8sCert.replace("\n", "\\n"));
             	templateObjects.put("k8s_newline_cert_win",k8sCert.replace("\n", "`n"));
+            } else {
+            	kubectTemplateLocal = kubectTemplateLocal.replace("--certificate-authority=\\$TMP_CERT", "--certificate-authority=/dev/null");
+            	
+            	if (winKubCtlLocal != null) {
+            		winKubCtlLocal = winKubCtlLocal.replace("\"$k8s_newline_cert_win$\" | out-file \\$TMP_CERT -encoding oem ;", "");
+            	}
             }
+            
+            
+            
             
             if (ouCert != null) {
             	templateObjects.put("ou_b64_cert",new String(Base64.encodeBase64(ouCert.getBytes("UTF-8"))));
-            }
+            	
+            } 
             
 
             
             
             
+            
+            
 
             
-            tokens.put("kubectl Command",this.renderTemplate(this.kubectlTemplate,templateObjects));
+            tokens.put("kubectl Command",this.renderTemplate(kubectTemplateLocal,templateObjects));
             
             if (this.kubectlWinUsage != null) {
-            	tokens.put("kubectl Windows Command",this.renderTemplate(this.kubectlWinUsage,templateObjects));
+            	tokens.put("kubectl Windows Command",this.renderTemplate(winKubCtlLocal,templateObjects));
             }
             
             tokens.put("Usage",this.kubectlUsage);
