81ef503bdd92559d93911f71f37701836fa4bda3
mlbiam <marc@tremolo.io>
2020-07-18 08:57:16 -0400

    for #462 watch on trusts

--- unison/unison-applications-k8s/src/main/java/com/tremolosecurity/oidc/k8s/K8sLoadTrusts.java
+++ unison/unison-applications-k8s/src/main/java/com/tremolosecurity/oidc/k8s/K8sLoadTrusts.java
@@ -13,13 +13,21 @@
 
 package com.tremolosecurity.oidc.k8s;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.HashSet;
 
 import javax.servlet.ServletContext;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import com.tremolosecurity.idp.providers.OpenIDConnectIdP;
 import com.tremolosecurity.idp.providers.OpenIDConnectTrust;
@@ -28,36 +36,53 @@ import com.tremolosecurity.provisioning.mapping.MapIdentity;
 import com.tremolosecurity.provisioning.util.HttpCon;
 import com.tremolosecurity.saml.Attribute;
 import com.tremolosecurity.server.GlobalEntries;
+import com.tremolosecurity.server.StopableThread;
 import com.tremolosecurity.unison.openshiftv3.OpenShiftTarget;
 
-public class K8sLoadTrusts implements DynamicLoadTrusts {
+public class K8sLoadTrusts implements DynamicLoadTrusts,StopableThread {
 	
 	static org.apache.logging.log4j.Logger logger = org.apache.logging.log4j.LogManager.getLogger(K8sLoadTrusts.class.getName());
 	
+	HashMap<String, OpenIDConnectTrust> trusts;
+
+	private String k8sTarget;
+
+	private String namespace;
+
+	private String uri;
+
+	private OpenShiftTarget k8s;
+	
+	private HashSet<String> resourceVersions;
+	
+	
+	
+	boolean keepRunning;
 	
 	@Override
 	public void loadTrusts(String idpName, ServletContext ctx,
 			HashMap<String, Attribute> init, HashMap<String, HashMap<String, Attribute>> trustCfg, MapIdentity mapper,HashMap<String, OpenIDConnectTrust> trusts)
 			throws Exception {
 		
-
+		this.trusts = trusts;
 		
-		String k8sTarget = 	init.get("trusts.k8starget").getValues().get(0);
-		String namespace = "openunison";//init.get("trusts.namespaces").getValues().get(0);
-		String uri = "/apis/openunison.tremolo.io/v1/namespaces/" + namespace + "/trusts";
+		this.k8sTarget = 	init.get("trusts.k8starget").getValues().get(0);
+		this.namespace = "openunison";//init.get("trusts.namespaces").getValues().get(0);
+		this.uri = "/apis/openunison.tremolo.io/v1/namespaces/" + this.namespace + "/trusts";
 		
-		OpenShiftTarget k8s = (OpenShiftTarget) GlobalEntries.getGlobalEntries().getConfigManager().getProvisioningEngine().getTarget(k8sTarget).getProvider();
+		this.k8s = (OpenShiftTarget) GlobalEntries.getGlobalEntries().getConfigManager().getProvisioningEngine().getTarget(k8sTarget).getProvider();
 		
-		if (k8s == null) {
+		if (this.k8s == null) {
 			throw new Exception("Target " + k8sTarget + " does not exist");
 		}
 		
-		HttpCon http = k8s.createClient();
+		HttpCon http = this.k8s.createClient();
+		
+		this.resourceVersions = new HashSet<String>();
 		
 		try {
 			String token = k8s.getAuthToken();
 			String json = k8s.callWS(token, http, uri);
-			System.out.println(json);
 			
 			JSONObject list = (JSONObject) new JSONParser().parse(json);
 			JSONArray items = (JSONArray) list.get("items");
@@ -68,66 +93,7 @@ public class K8sLoadTrusts implements DynamicLoadTrusts {
 			}
 			
 			for (Object o : items) {
-				JSONObject trustObj = (JSONObject) o;
-				JSONObject metadata = (JSONObject) trustObj.get("metadata");
-				JSONObject spec = (JSONObject) trustObj.get("spec");
-				logger.info(metadata.get("name"));
-				
-				
-				OpenIDConnectTrust trust = new OpenIDConnectTrust();
-				trust.setClientID(spec.get("clientId").toString());
-				trust.setPublicEndpoint((Boolean) spec.get("publicEndpoint"));
-				
-				if (! trust.isPublicEndpoint()) {
-					JSONObject secretInfo = (JSONObject) spec.get("clientSecret");
-					if (secretInfo == null) {
-						logger.error("secretInfo not provided for trust " + metadata.get("name"));
-						continue;
-					}
-					String secretUri = "/api/v1/namespaces/" + namespace + "/secrets/" + secretInfo.get("secretName").toString();
-					String secretJson = k8s.callWS(token, http, secretUri);
-					JSONObject secret = (JSONObject) new JSONParser().parse(secretJson);
-					
-					JSONObject data = (JSONObject) secret.get("data");
-					if (data == null) {
-						logger.error("Invalid secret response for " + secretUri + " - " + secretJson);
-						continue;
-					}
-					
-					
-					
-					String secretData = (String) data.get(secretInfo.get("keyName").toString());
-					
-					if (secretData == null) {
-						logger.error("Secret key " + secretInfo.get("keyName").toString() + " does not exist");
-						continue;
-					}
-					
-					
-					
-					String decoded = new String(java.util.Base64.getDecoder().decode(secretData));
-					
-					
-					trust.setClientSecret(decoded);
-				}
-				
-				
-				
-				trust.setRedirectURI(spec.get("redirectURI").toString());
-				trust.setCodeLastmileKeyName(spec.get("codeLastMileKeyName").toString());
-				trust.setAuthChain(spec.get("authChainName").toString());
-				trust.setCodeTokenTimeToLive((Long) spec.get("codeTokenSkewMilis"));
-				trust.setAccessTokenTimeToLive((Long) spec.get("accessTokenTimeToLive"));
-				trust.setAccessTokenSkewMillis((Long) spec.get("accessTokenSkewMillis"));
-
-				
-				trust.setSignedUserInfo((Boolean) spec.get("signedUserInfo"));
-				trust.setVerifyRedirect((Boolean) spec.get("verifyRedirect"));
-				
-				
-				trust.setTrustName(metadata.get("name").toString());
-				
-				trusts.put(trust.getClientID(),trust);
+				addTrust(trusts, http, token, o);
 				
 				
 			}
@@ -137,7 +103,161 @@ public class K8sLoadTrusts implements DynamicLoadTrusts {
 			http.getBcm().close();
 		}
 		
+		this.keepRunning = true;
+		logger.info("Adding stoppable thread");
+		GlobalEntries.getGlobalEntries().getConfigManager().addThread(this);
+		logger.info("Starting watch");
+		new Thread(this).start();
 		
+	}
+
+	private void addTrust(HashMap<String, OpenIDConnectTrust> trusts, HttpCon http, String token, Object o)
+			throws IOException, ClientProtocolException, ParseException {
+		JSONObject trustObj = (JSONObject) o;
+		JSONObject metadata = (JSONObject) trustObj.get("metadata");
+		
+		
+		String resourceVersion = (String) metadata.get("resourceVersion");
+		
+		if (this.resourceVersions.contains(resourceVersion)) {
+			logger.info("Resource " + resourceVersion + " already processed, skipping");
+			return;
+		}
+		
+		this.resourceVersions.add(resourceVersion);
+		
+		JSONObject spec = (JSONObject) trustObj.get("spec");
+		logger.info(metadata.get("name"));
+		
+		
+		OpenIDConnectTrust trust = new OpenIDConnectTrust();
+		trust.setClientID(spec.get("clientId").toString());
+		trust.setPublicEndpoint((Boolean) spec.get("publicEndpoint"));
+		
+		if (! trust.isPublicEndpoint()) {
+			JSONObject secretInfo = (JSONObject) spec.get("clientSecret");
+			if (secretInfo == null) {
+				logger.error("secretInfo not provided for trust " + metadata.get("name"));
+				return;
+			}
+			String secretUri = "/api/v1/namespaces/" + namespace + "/secrets/" + secretInfo.get("secretName").toString();
+			String secretJson = k8s.callWS(token, http, secretUri);
+			JSONObject secret = (JSONObject) new JSONParser().parse(secretJson);
+			
+			JSONObject data = (JSONObject) secret.get("data");
+			if (data == null) {
+				logger.error("Invalid secret response for " + secretUri + " - " + secretJson);
+				return;
+			}
+			
+			
+			
+			String secretData = (String) data.get(secretInfo.get("keyName").toString());
+			
+			if (secretData == null) {
+				logger.error("Secret key " + secretInfo.get("keyName").toString() + " does not exist");
+				return;
+			}
+			
+			
+			
+			String decoded = new String(java.util.Base64.getDecoder().decode(secretData));
+			
+			
+			trust.setClientSecret(decoded);
+		}
+		
+		
+		
+		trust.setRedirectURI(spec.get("redirectURI").toString());
+		trust.setCodeLastmileKeyName(spec.get("codeLastMileKeyName").toString());
+		trust.setAuthChain(spec.get("authChainName").toString());
+		trust.setCodeTokenTimeToLive((Long) spec.get("codeTokenSkewMilis"));
+		trust.setAccessTokenTimeToLive((Long) spec.get("accessTokenTimeToLive"));
+		trust.setAccessTokenSkewMillis((Long) spec.get("accessTokenSkewMillis"));
+
+		
+		trust.setSignedUserInfo((Boolean) spec.get("signedUserInfo"));
+		trust.setVerifyRedirect((Boolean) spec.get("verifyRedirect"));
+		
+		
+		trust.setTrustName(metadata.get("name").toString());
+		
+		synchronized(trusts) {
+			trusts.put(trust.getClientID(),trust);
+		}
+	}
+
+	@Override
+	public void run() {
+		logger.info("Starting watch");
+		while (this.keepRunning) {
+			HttpCon http;
+			try {
+				http = this.k8s.createClient();
+			} catch (Exception e1) {
+				logger.error("Could not create connection",e1);
+				return;
+			}
+			
+			try {
+				String url = new StringBuilder().append(this.k8s.getUrl())
+						                        .append(this.uri)
+						                        .append("?watch=true&timeoutSeconds=10").toString();
+				logger.info("watching " + url);
+				HttpGet get = new HttpGet(url);
+				get.setHeader("Authorization", new StringBuilder().append("Bearer ").append(this.k8s.getAuthToken()).toString());
+				HttpResponse resp = http.getHttp().execute(get);
+				BufferedReader in = new BufferedReader(new InputStreamReader(resp.getEntity().getContent()));
+				String line = null;
+				
+				HttpCon nonwatchHttp = this.k8s.createClient();
+				
+				while ((line = in.readLine()) != null) {
+					JSONObject event = (JSONObject) new JSONParser().parse(line);
+					String action = (String) event.get("type");
+					JSONObject trust = (JSONObject) event.get("object");
+					
+					
+					
+					if (action.equalsIgnoreCase("ADDED") || action.equalsIgnoreCase("MODIFIED")) {
+						this.addTrust(trusts, nonwatchHttp, this.k8s.getAuthToken(), trust);
+					} else {
+						//deleted
+						JSONObject metadata = (JSONObject) trust.get("metadata");
+						String name = (String) metadata.get("name");
+						logger.info("Deleting trust " + name);
+						JSONObject spec = (JSONObject) trust.get("spec");
+						String clientId = (String) spec.get("clientId");
+						synchronized(trusts) {
+							trusts.remove(clientId);
+						}
+					}
+				}
+				
+				nonwatchHttp.getHttp().close();
+				nonwatchHttp.getBcm().close();
+				
+			} catch (Exception e) {
+				logger.error("Could not get authentication token",e);
+				return;
+			} finally {
+				if (http != null) {
+					try {
+						http.getHttp().close();
+					} catch (IOException e) {
+						
+					}
+					http.getBcm().close();
+				}
+			}
+		}
+		
+	}
+
+	@Override
+	public void stop() {
+		this.keepRunning = false;
 		
 	}
 
