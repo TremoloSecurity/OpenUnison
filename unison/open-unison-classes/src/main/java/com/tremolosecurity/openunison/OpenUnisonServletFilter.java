cba0625957187db19ba7d584e6e4d1432f21e88a
mlbiam <marc@tremolo.io>
2020-09-03 08:28:15 -0400

    for #474 bumped version numbers

--- unison/open-unison-classes/src/main/java/com/tremolosecurity/openunison/OpenUnisonServletFilter.java
+++ unison/open-unison-classes/src/main/java/com/tremolosecurity/openunison/OpenUnisonServletFilter.java
@@ -47,7 +47,7 @@ public class OpenUnisonServletFilter extends UnisonServletFilter {
 
 
 
-	public static final String version = "1.0.20-2020082001";
+	public static final String version = "1.0.21-2020090301";
 
 
 
