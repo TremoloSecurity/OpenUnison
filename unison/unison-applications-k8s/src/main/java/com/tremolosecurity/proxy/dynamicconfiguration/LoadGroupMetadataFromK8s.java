f5ad512dbaab5e48a5a9ed969c86681e5e3b8204
mlbiam <marc@tremolo.io>
2021-10-05 09:18:21 -0400

    for #577 created watch

---
+++ unison/unison-applications-k8s/src/main/java/com/tremolosecurity/proxy/dynamicconfiguration/LoadGroupMetadataFromK8s.java
@@ -0,0 +1,215 @@
+/*******************************************************************************
+ * Copyright 2021 Tremolo Security, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package com.tremolosecurity.proxy.dynamicconfiguration;
+
+import java.util.Map;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.tremolosecurity.config.util.ConfigManager;
+import com.tremolosecurity.config.xml.AuthChainType;
+import com.tremolosecurity.config.xml.ConfigType;
+import com.tremolosecurity.config.xml.CustomAzRuleType;
+import com.tremolosecurity.config.xml.MechanismType;
+import com.tremolosecurity.config.xml.ParamListType;
+import com.tremolosecurity.config.xml.ParamType;
+import com.tremolosecurity.config.xml.ResultGroupType;
+import com.tremolosecurity.config.xml.ResultType;
+import com.tremolosecurity.config.xml.TargetType;
+import com.tremolosecurity.config.xml.TremoloType;
+import com.tremolosecurity.k8s.watch.K8sWatchTarget;
+import com.tremolosecurity.k8s.watch.K8sWatcher;
+import com.tremolosecurity.openunison.util.config.OpenUnisonConfigLoader;
+import com.tremolosecurity.provisioning.core.ProvisioningEngine;
+import com.tremolosecurity.provisioning.core.ProvisioningException;
+import com.tremolosecurity.proxy.dynamicloaders.DynamicAuthMechs;
+import com.tremolosecurity.proxy.dynamicloaders.DynamicAuthorizations;
+import com.tremolosecurity.proxy.dynamicloaders.DynamicResultGroups;
+import com.tremolosecurity.proxy.filters.SetupGroupMetadataWatch;
+import com.tremolosecurity.provisioning.targets.LoadTargetsFromK8s;
+import com.tremolosecurity.provisioning.util.HttpCon;
+import com.tremolosecurity.saml.Attribute;
+import com.tremolosecurity.server.GlobalEntries;
+
+public class LoadGroupMetadataFromK8s  implements  K8sWatchTarget {
+	static org.apache.logging.log4j.Logger logger = org.apache.logging.log4j.LogManager.getLogger(LoadGroupMetadataFromK8s.class.getName());
+	
+	K8sWatcher k8sWatch;
+	
+	TremoloType tremolo;
+
+	
+	private ConfigManager cfgMgr;
+	
+	private SetupGroupMetadataWatch md;
+	
+	
+	
+	
+
+	
+	
+	
+	public void loadGroupMetadatas(ConfigManager cfgMgr, String k8sTarget, String namespace,SetupGroupMetadataWatch md) throws ProvisioningException {
+		this.tremolo = cfgMgr.getCfg();
+		String uri = "/apis/openunison.tremolo.io/v1/namespaces/" + namespace + "/groupmetadatas";
+		
+		this.md = md;
+		
+		this.cfgMgr = cfgMgr;
+		
+		this.k8sWatch = new K8sWatcher(k8sTarget,namespace,uri,this,cfgMgr,cfgMgr.getProvisioningEngine());
+		
+		this.k8sWatch.initalRun();
+
+	}
+
+
+
+	@Override
+	public void addObject(TremoloType cfg, JSONObject item) throws ProvisioningException {
+		String rawJson = item.toJSONString();
+		StringBuffer b = new StringBuffer();
+		b.setLength(0);
+		OpenUnisonConfigLoader.integrateIncludes(b,rawJson);
+		try {
+			JSONObject newRoot = (JSONObject) new JSONParser().parse(b.toString());
+			JSONObject metadata = (JSONObject) newRoot.get("metadata");
+			
+			if (metadata == null) {
+				throw new ProvisioningException("No metadata");
+			}
+			
+			String name = (String) metadata.get("name");
+			
+			logger.info("Adding GroupMetadata " + name);
+			
+			
+			try {
+				JSONObject spec = (JSONObject) newRoot.get("spec");
+				String k8s = (String) spec.get("groupName");
+				String ext = (String) spec.get("externalName");
+				
+				this.md.addMapping(k8s, ext);
+				
+			} catch (Exception e) {
+				logger.warn("Could not initialize group mapping " + name,e);
+				return;
+			}
+			
+			
+			
+			
+		} catch (ParseException e) {
+			throw new ProvisioningException("Could not parse custom groupmetadata",e);
+		}
+		
+	}
+
+
+
+	@Override
+	public void modifyObject(TremoloType cfg, JSONObject item) throws ProvisioningException {
+		String rawJson = item.toJSONString();
+		StringBuffer b = new StringBuffer();
+		b.setLength(0);
+		OpenUnisonConfigLoader.integrateIncludes(b,rawJson);
+		try {
+			JSONObject newRoot = (JSONObject) new JSONParser().parse(b.toString());
+			JSONObject metadata = (JSONObject) newRoot.get("metadata");
+			
+			if (metadata == null) {
+				throw new ProvisioningException("No metadata");
+			}
+			
+			String name = (String) metadata.get("name");
+			
+			logger.info("Modifying GroupMetadata " + name);
+			
+			
+			try {
+				JSONObject spec = (JSONObject) newRoot.get("spec");
+				String k8s = (String) spec.get("groupName");
+				String ext = (String) spec.get("externalName");
+				
+				
+				
+				this.md.addMapping(k8s, ext);
+				
+			} catch (Exception e) {
+				logger.warn("Could not initialize group mapping " + name,e);
+				return;
+			}
+			
+			
+			
+			
+		} catch (ParseException e) {
+			throw new ProvisioningException("Could not parse groupmetadata",e);
+		}
+		
+	}
+
+
+
+	@Override
+	public void deleteObject(TremoloType cfg, JSONObject item) throws ProvisioningException {
+		String rawJson = item.toJSONString();
+		StringBuffer b = new StringBuffer();
+		b.setLength(0);
+		OpenUnisonConfigLoader.integrateIncludes(b,rawJson);
+		try {
+			JSONObject newRoot = (JSONObject) new JSONParser().parse(b.toString());
+			JSONObject metadata = (JSONObject) newRoot.get("metadata");
+			
+			if (metadata == null) {
+				throw new ProvisioningException("No metadata");
+			}
+			
+			String name = (String) metadata.get("name");
+			
+			logger.info("Deleting GroupMetadata " + name);
+			
+			
+			try {
+				JSONObject spec = (JSONObject) newRoot.get("spec");
+				String k8s = (String) spec.get("groupName");
+				String ext = (String) spec.get("externalName");
+				
+				
+				
+				this.md.deleteMapping(k8s, ext);
+				
+			} catch (Exception e) {
+				logger.warn("Could not initialize group mapping " + name,e);
+				return;
+			}
+			
+			
+			
+			
+		} catch (ParseException e) {
+			throw new ProvisioningException("Could not parse groupmetadata",e);
+		}
+		
+		
+	}
+	
+	
+}
---
+++ unison/unison-applications-k8s/src/main/java/com/tremolosecurity/proxy/filters/SetupGroupMetadataWatch.java
@@ -0,0 +1,140 @@
+/*******************************************************************************
+ * Copyright 2021 Tremolo Security, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package com.tremolosecurity.proxy.filters;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.novell.ldap.util.DN;
+import com.tremolosecurity.proxy.dynamicconfiguration.LoadGroupMetadataFromK8s;
+import com.tremolosecurity.proxy.filter.HttpFilter;
+import com.tremolosecurity.proxy.filter.HttpFilterChain;
+import com.tremolosecurity.proxy.filter.HttpFilterConfig;
+import com.tremolosecurity.proxy.filter.HttpFilterRequest;
+import com.tremolosecurity.proxy.filter.HttpFilterResponse;
+
+
+
+public class SetupGroupMetadataWatch implements HttpFilter {
+
+	static Map<String,String> ext2k8s;
+	static Map<String,String> k8s2ext;
+	
+	
+	
+	boolean extIsDN;
+	String target;
+	String namespace;
+
+
+
+	@Override
+	public void doFilter(HttpFilterRequest request, HttpFilterResponse response, HttpFilterChain chain)
+			throws Exception {
+		chain.nextFilter(request, response, chain);
+		
+	}
+
+
+
+	@Override
+	public void filterResponseText(HttpFilterRequest request, HttpFilterResponse response, HttpFilterChain chain,
+			StringBuffer data) throws Exception {
+
+		
+	}
+
+
+
+	@Override
+	public void filterResponseBinary(HttpFilterRequest request, HttpFilterResponse response, HttpFilterChain chain,
+			byte[] data, int length) throws Exception {
+
+		
+	}
+
+
+
+	@Override
+	public void initFilter(HttpFilterConfig config) throws Exception {
+		this.extIsDN = config.getAttribute("extIsDN").getValues().get(0).equalsIgnoreCase("true");
+		this.target = config.getAttribute("target").getValues().get(0);
+		this.namespace = config.getAttribute("namespace").getValues().get(0);
+		
+		ext2k8s = new HashMap<String,String>();
+		k8s2ext = new HashMap<String,String>();
+		
+		LoadGroupMetadataFromK8s lgm = new LoadGroupMetadataFromK8s();
+		lgm.loadGroupMetadatas(config.getConfigManager(),target,namespace,this);
+		
+	}
+	
+	public static String getK8s(String ext) {
+		return ext2k8s.get(ext.toLowerCase()); 
+	}
+	
+	public static String getExt(String k8s) {
+		return k8s2ext.get(k8s.toLowerCase());
+	}
+	
+	
+	public synchronized void addMapping(String k8s,String ext) {
+		
+		ext = ext.toLowerCase();
+		k8s = k8s.toLowerCase();
+		
+		if (extIsDN) {
+			DN dn = new DN(ext);
+			ext = dn.toString();
+		}
+		
+		ext2k8s.put(ext, k8s);
+		k8s2ext.put(k8s, ext);
+	}
+	
+	public synchronized void deleteMapping(String k8s,String ext) {
+		ext = ext.toLowerCase();
+		k8s = k8s.toLowerCase();
+		
+		if (extIsDN) {
+			DN dn = new DN(ext);
+			ext = dn.toString();
+		}
+		
+		String keyToDel = null;
+		for (String key : ext2k8s.keySet()) {
+			if (ext2k8s.get(key).equalsIgnoreCase(k8s)) {
+				keyToDel = key;
+			}
+		}
+		
+		ext2k8s.remove(keyToDel);
+		
+		keyToDel = null;
+		
+		for (String key : k8s2ext.keySet()) {
+			if (k8s2ext.get(key).equalsIgnoreCase(ext)) {
+				keyToDel = key;
+			}
+		}
+		
+		k8s2ext.remove(keyToDel);
+	}
+	
+	
+	
+}
