d1bdf2081191197ba7a703f79a367e00af7f395e
mlbiam <marc@tremolo.io>
2022-02-09 14:12:07 -0500

    for #615 updated versions

--- unison/open-unison-classes/src/main/java/com/tremolosecurity/openunison/OpenUnisonServletFilter.java
+++ unison/open-unison-classes/src/main/java/com/tremolosecurity/openunison/OpenUnisonServletFilter.java
@@ -47,7 +47,7 @@ public class OpenUnisonServletFilter extends UnisonServletFilter {
 
 
 
-	public static final String version = "1.0.25-2022012901";
+	public static final String version = "1.0.26-2022020901";
 
 
 
