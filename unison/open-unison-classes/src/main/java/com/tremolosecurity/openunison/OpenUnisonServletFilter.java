be403646b6f1a1c308060d590cadf858ae0d5ec9
mlbiam <marc@tremolo.io>
2022-02-12 21:14:26 -0500

    for #616 bumped myvd to 1.0.13

--- unison/open-unison-classes/src/main/java/com/tremolosecurity/openunison/OpenUnisonServletFilter.java
+++ unison/open-unison-classes/src/main/java/com/tremolosecurity/openunison/OpenUnisonServletFilter.java
@@ -47,7 +47,7 @@ public class OpenUnisonServletFilter extends UnisonServletFilter {
 
 
 
-	public static final String version = "1.0.26-2022020901";
+	public static final String version = "1.0.27-2022021201";
 
 
 
