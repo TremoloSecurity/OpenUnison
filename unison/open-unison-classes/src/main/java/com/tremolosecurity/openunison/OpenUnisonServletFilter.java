855518a1bcda5811aae929ce00c8070b4d1bf640
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2024-06-05 17:12:52 -0400

    for #891 version bump

--- unison/open-unison-classes/src/main/java/com/tremolosecurity/openunison/OpenUnisonServletFilter.java
+++ unison/open-unison-classes/src/main/java/com/tremolosecurity/openunison/OpenUnisonServletFilter.java
@@ -47,7 +47,7 @@ public class OpenUnisonServletFilter extends UnisonServletFilter {
 
 
 
-	public static final String version = "1.0.40-2024030801";
+	public static final String version = "1.0.41-2024030801";
 
 
 
