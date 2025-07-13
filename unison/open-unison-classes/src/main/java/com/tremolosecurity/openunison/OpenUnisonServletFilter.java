4f7f652d1b74be76ba279050b28e85f04e048ee3
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2025-07-13 14:46:48 -0400

    for #957 revert json-io by a version, some serialization and deserialization failing

--- unison/open-unison-classes/src/main/java/com/tremolosecurity/openunison/OpenUnisonServletFilter.java
+++ unison/open-unison-classes/src/main/java/com/tremolosecurity/openunison/OpenUnisonServletFilter.java
@@ -47,7 +47,7 @@ public class OpenUnisonServletFilter extends UnisonServletFilter {
 
 
 
-	public static final String version = "1.0.43-2025071301";
+	public static final String version = "1.0.43-2025071302";
 
 
 
