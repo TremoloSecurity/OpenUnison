4af389ab2b83af1324aa7344fbbb52b7eb408db8
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2026-01-14 18:49:10 -0500

    for #1034 added more utilities

--- unison/unison-sdk/src/main/java/com/tremolosecurity/util/JSUtils.java
+++ unison/unison-sdk/src/main/java/com/tremolosecurity/util/JSUtils.java
@@ -15,15 +15,29 @@
  *******************************************************************************/
 package com.tremolosecurity.util;
 
+import com.novell.ldap.LDAPAttribute;
+import com.novell.ldap.LDAPEntry;
+import com.novell.ldap.LDAPException;
+import com.tremolosecurity.provisioning.core.Group;
+import com.tremolosecurity.provisioning.core.User;
+import com.tremolosecurity.server.GlobalEntries;
+import net.sourceforge.myvd.types.Filter;
+import org.apache.directory.api.ldap.model.filter.FilterParser;
+import org.apache.log4j.Logger;
+
+
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.net.http.HttpRequest;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 /**
  * Utility class because graalvm js doesn't play well with Java String<->byte[]
  */
 public class JSUtils {
+
+	static Logger logger = Logger.getLogger(JSUtils.class);
 	public static byte[] string2bytes(String s) {
 		try {
 			return s.getBytes("UTF-8");
@@ -48,5 +62,62 @@ public class JSUtils {
 
 		String formPost = String.format(template,transformed);
 		return HttpRequest.BodyPublishers.ofString(formPost);
+	}
+
+	public static boolean checkUserAgainstFilter(User user,String memberOfAttribute,String filter) {
+		LDAPEntry ldap = new LDAPEntry("cn=x");
+		ldap.getAttributeSet().add(new LDAPAttribute("objectClass",GlobalEntries.getGlobalEntries().getConfigManager().getCfg().getUserObjectClass()));
+		user.getAttribs().keySet().forEach(attr -> {
+			LDAPAttribute ldapAttribute = new LDAPAttribute(attr);
+			user.getAttribs().get(attr).getValues().forEach(val -> {
+				ldapAttribute.addValue(val.getBytes(StandardCharsets.UTF_8));
+			});
+			ldap.getAttributeSet().add(ldapAttribute);
+		});
+
+		LDAPAttribute ldapGroups = new LDAPAttribute(memberOfAttribute);
+		user.getGroups().forEach(group -> {
+			ldapGroups.addValue(group.getBytes(StandardCharsets.UTF_8));
+		});
+		if (ldapGroups.getAllValues().size() > 0) {
+			ldap.getAttributeSet().add(ldapGroups);
+		}
+
+        try {
+            Filter parsedFilter = new Filter(filter);
+			return parsedFilter.getRoot().checkEntry(ldap);
+        } catch (LDAPException e) {
+            logger.warn("Could not parse filter",e);
+			return false;
+        }
+    }
+
+	public static boolean checkGroupAgainstFilter(Group group, String filter) {
+		LDAPEntry ldap = new LDAPEntry("cn=x");
+		ldap.getAttributeSet().add(new LDAPAttribute("cn",group.getName()));
+		ldap.getAttributeSet().add(new LDAPAttribute("objectClass",GlobalEntries.getGlobalEntries().getConfigManager().getCfg().getGroupObjectClass()));
+		group.getAttributes().keySet().forEach(attr -> {
+			LDAPAttribute ldapAttribute = new LDAPAttribute(attr);
+			group.getAttributes().get(attr).getValues().forEach(val -> {
+				ldapAttribute.addValue(val.getBytes(StandardCharsets.UTF_8));
+			});
+			ldap.getAttributeSet().add(ldapAttribute);
+		});
+
+		LDAPAttribute ldapGroups = new LDAPAttribute(GlobalEntries.getGlobalEntries().getConfigManager().getCfg().getGroupMemberAttribute());
+		group.getMembers().forEach(member -> {
+			ldapGroups.addValue(member.getBytes(StandardCharsets.UTF_8));
+		});
+		if (ldapGroups.getAllValues().size() > 0) {
+			ldap.getAttributeSet().add(ldapGroups);
+		}
+
+		try {
+			Filter parsedFilter = new Filter(filter);
+			return parsedFilter.getRoot().checkEntry(ldap);
+		} catch (LDAPException e) {
+			logger.warn("Could not parse filter",e);
+			return false;
+		}
 	}
 }
