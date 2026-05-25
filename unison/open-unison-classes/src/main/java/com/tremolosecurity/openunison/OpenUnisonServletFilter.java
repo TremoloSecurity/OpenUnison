de0db6ef3a547527d11f438290c5859646a9812f
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2026-05-25 12:46:23 -0400

    for #1076 removed xerces for opensaml, set build

--- unison/open-unison-classes/src/main/java/com/tremolosecurity/openunison/OpenUnisonServletFilter.java
+++ unison/open-unison-classes/src/main/java/com/tremolosecurity/openunison/OpenUnisonServletFilter.java
@@ -49,7 +49,7 @@ public class OpenUnisonServletFilter extends UnisonServletFilter {
 
 
 
-	public static final String version = "1.0.49-2026051501";
+	public static final String version = "1.0.50-2026052501";
 
 
 
