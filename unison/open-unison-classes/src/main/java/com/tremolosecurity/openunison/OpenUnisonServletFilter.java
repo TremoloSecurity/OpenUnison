86ce9d86460e17338a3d68cf2824fbb1c7833395
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2026-07-09 13:56:07 -0400

    for #1084 increement build numbers

--- unison/open-unison-classes/src/main/java/com/tremolosecurity/openunison/OpenUnisonServletFilter.java
+++ unison/open-unison-classes/src/main/java/com/tremolosecurity/openunison/OpenUnisonServletFilter.java
@@ -49,7 +49,7 @@ public class OpenUnisonServletFilter extends UnisonServletFilter {
 
 
 
-	public static final String version = "1.0.50-2026052501";
+	public static final String version = "1.0.51-2026070901";
 
 
 
